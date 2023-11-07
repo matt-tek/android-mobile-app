55a2517b015c1671052446e687c4c32e3ffcff2a
matt-tek <mathieu.muty@epitech.eu>
2023-11-07 01:12:48 -0500

    [FEAT] add todo module with basic CRUD to create and get todos data

--- node-back/src/app.module.ts
+++ node-back/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModuleModule } from './todo-module/todo-module.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TodoModuleModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('MYSQL_HOST'),
+        port: parseInt(configService.get('MYSQL_TCP_PORT')),
+        username: configService.get('MYSQL_USER'),
+        password: configService.get('MYSQL_ROOT_PASSWORD'),
+        database: configService.get('MYSQL_TODO_DATABASE'),
+        entities: ['dist/**/*.entity.js'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
