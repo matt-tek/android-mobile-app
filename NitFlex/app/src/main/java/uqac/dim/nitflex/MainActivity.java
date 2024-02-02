6145c8d296caeb1e56cc5de818a1e892971d0ee8
matt-tek <mathieu.muty@epitech.eu>
2024-02-01 21:21:02 -0500

    [ADD] menu on app

--- NitFlex/app/src/main/java/uqac/dim/nitflex/MainActivity.java
+++ NitFlex/app/src/main/java/uqac/dim/nitflex/MainActivity.java
@@ -1,6 +1,10 @@
 package uqac.dim.nitflex;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    public void printMessage(View view) {
+        Log.i("DIM", "Nitflex Log !");
+    }
+
+    public void eventItem1(MenuItem item) {
+        Log.i("DIM", "Item 1 selected !");
+    }
+
+    public void eventItem2(MenuItem item) {
+        Log.i("DIM", "Item 2 selected !");
+    }
 }
